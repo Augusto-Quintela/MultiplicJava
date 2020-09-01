b18a1948c9ca864f12b9a17cd9ffd63cae86bc0d
Augusto-Quintela <65916601+Augusto-Quintela@users.noreply.github.com>
2020-09-01 19:51:24 -0300

    Add files via upload

---
+++ Multiplicacao.java
@@ -0,0 +1,23 @@
+  
+package PacoteJava;
+
+import javax.swing.*;
+
+public class Multiplicacao{
+    
+    public static void main(String args[]){
+        double num1,num2, resp=0;
+        String aux="";
+        
+    aux = JOptionPane.showInputDialog(null,"Insira o primeiro número");
+    num1 = Double.parseDouble(aux);
+    
+    aux = JOptionPane.showInputDialog(null,"Insira o segundo número");
+    num2 = Double.parseDouble(aux);
+    
+    resp = num1*num2;
+    
+    JOptionPane.showMessageDialog(null, "O resultado da multiplicação é "+resp);
+    }
+    
+}
